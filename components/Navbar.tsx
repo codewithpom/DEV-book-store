e1c123a59ba55c491240a27c3f329ec8a5cc52f5
codewithpom <codewithpom@gmail.com>
2022-01-04 15:59:50 +0530

    Added home link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -9,11 +11,14 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <div className="nav-link">
+                            <Link href="/">
+                                <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+
+                            </Link>
+                        </div>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
+
 
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
