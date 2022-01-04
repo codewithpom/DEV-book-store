8f398d848de0bc20ad0ac1736390dcb4129400e8
codewithpom <codewithpom@gmail.com>
2022-01-04 15:22:57 +0530

    Put bootstrap css down to increase load time

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,8 @@
-import Head from "next/head";
 import Navbar from "../components/Navbar";
 import "../styles/global.css";
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Head>
-
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-                <link
-                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-                    rel="stylesheet"
-                    crossOrigin="anonymous"
-                />
-            </Head>
-
-
             <Navbar />
             <Component {...pageProps} />
             <script
@@ -24,6 +11,11 @@ function MyApp({ Component, pageProps }) {
 
             <script
                 src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+            />
+            <link
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+                rel="stylesheet"
+                crossOrigin="anonymous"
             />
         </>
     );
