d1cad64e0aa5ae1bb3620b2bfcf9be2cbe197ea8
codewithpom <codewithpom@gmail.com>
2022-01-04 15:48:22 +0530

    Added loading progress bar to make it look cool  😎

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import "../styles/global.css";
+import NextNProgress from "nextjs-progressbar";
+
 function MyApp({ Component, pageProps }) {
     return (
         <>
@@ -15,8 +17,10 @@ function MyApp({ Component, pageProps }) {
                 />
             </Head>
 
+            <NextNProgress />
 
             <Navbar />
+
             <Component {...pageProps} />
             <script
                 src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
